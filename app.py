516b126a1bb5e194bceadf2b4e809f74356c5771
Adam A <aaa237@njit.edu>
2020-11-04 23:22:54 +0000

    Set up landing page with react/database boilerplate

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import flask_sqlalchemy
 import flask_socketio
 import models 
 
-ADDRESSES_RECEIVED_CHANNEL = 'addresses received'
-
 app = flask.Flask(__name__)
 
 socketio = flask_socketio.SocketIO(app)
@@ -25,47 +23,12 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 db.init_app(app)
 db.app = app
 
-
 db.create_all()
 db.session.commit()
 
-def emit_all_addresses(channel):
-    all_addresses = [ \
-        db_address.address for db_address \
-        in db.session.query(models.Usps).all()]
-        
-    socketio.emit(channel, {
-        'allAddresses': all_addresses
-    })
-
-
-@socketio.on('connect')
-def on_connect():
-    print('Someone connected!')
-    socketio.emit('connected', {
-        'test': 'Connected'
-    })
-    
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
-    
-
-@socketio.on('disconnect')
-def on_disconnect():
-    print ('Someone disconnected!')
-
-@socketio.on('new address input')
-def on_new_address(data):
-    print("Got an event for new address input with data:", data)
-    
-    db.session.add(models.Usps(data["address"]));
-    db.session.commit();
-    
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
 
 @app.route('/')
 def index():
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
-
     return flask.render_template("index.html")
 
 if __name__ == '__main__': 
--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,32 +1,12 @@
-    
 import * as React from 'react';
-
-
-import { Button } from './Button';
 import { Socket } from './Socket';
 
 export function Content() {
-    const [addresses, setAddresses] = React.useState([]);
-    
-    function getNewAddresses() {
-        React.useEffect(() => {
-            Socket.on('addresses received', (data) => {
-                console.log("Received addresses from server: " + data['allAddresses']);
-                setAddresses(data['allAddresses']);
-            })
-        });
-    }
-    
-    getNewAddresses();
 
     return (
         <div>
-            <h1>USPS Addresses!</h1>
-                <ol>
-                    {addresses.map((address, index) =>
-                        <li key={index}>{address}</li>)}
-                </ol>
-            <Button />
+            <h1>Syllendar Landing Page </h1>
+            <p></p>
         </div>
     );
 }
