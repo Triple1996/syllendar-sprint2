9ea6843d5f43de28f305920a1fe074f0fe625b1c
Adam A <aaa237@njit.edu>
2020-11-04 23:26:24 +0000

    Add comment to landing page

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -6,7 +6,7 @@ export function Content() {
     return (
         <div>
             <h1>Syllendar Landing Page </h1>
-            <p></p>
+            <p>This page should be a login that will take us to the main page</p>
         </div>
     );
 }
