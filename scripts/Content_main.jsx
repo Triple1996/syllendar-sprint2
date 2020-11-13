7ab7ffdf21a02a6973c7bc3224864503bfb9230f
Adam A <aaa237@njit.edu>
2020-11-13 19:59:26 +0000

    Consolidate most sockets so we do not have unnecessary emits

--- scripts/Content_main.jsx
+++ scripts/Content_main.jsx
@@ -1,4 +1,3 @@
-   
 import * as React from 'react';
 import { Socket } from './Socket';
 import ReactDOM from 'react-dom';
@@ -9,42 +8,21 @@ export function Content_main () {
     const [image, setImage] = React.useState([]);
     const [email, setEmail] = React.useState([]);
     const [name, setName] = React.useState([]);
-    
-    function getImage() {
-        React.useEffect(() => {
-            Socket.on('imageLinks', (data) => {
-                 console.log("Received user image from server: " + data['image']);
-                 setImage(data['image']);
-            })
-        });
-    }
-    getImage();
-    
-    function getEmail() {
-        React.useEffect(() => {
-            Socket.on('emailAddress', (data) => {
-                console.log("Received user email address from server: " + data['email']);
-                setEmail(data['email']);
-            })
-        });
-    }
-    getEmail();
-    
-    function getName() {
-        React.useEffect(() => {
-            Socket.on('realname', (data) => {
-                 console.log("Received user name from server: " + data['name']);
-                 setName(data['name']);
-            })
-        });
-    }
-    getName();
-    
+
+    React.useEffect(() => {
+        Socket.on('userinfo', (data) => {
+            console.log("Received user image from server: " + data['imageLink']);
+            setImage(data['image']);
+            console.log("Received user email address from server: " + data['email']);
+            setEmail(data['email']);
+            console.log("Received user name from server: " + data['name']);
+            setName(data['name']);
+        })
+    });
+
     function handleSubmit(event) {
-    
-     ReactDOM.render(<Content />, document.getElementById('content'));
-}
-    
+        ReactDOM.render(<Content />, document.getElementById('content'));
+    }
 
     return (
         <div>
