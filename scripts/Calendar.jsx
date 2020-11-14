a52f2148d57f810d814a230c24180c50e582b216
EC2 Default User <ec2-user@ip-172-31-70-13.ec2.internal>
2020-11-14 01:30:29 +0000

    Fix calendar starting on wrong day issue

--- scripts/Calendar.jsx
+++ scripts/Calendar.jsx
@@ -6,24 +6,13 @@ export default function Calendar() {
   const { isShowing, toggle } = useModal();
   const [date, setDate] = useState('');
 
-  const y = [];
-  const currentTime = new Date();
-  const currentDate = currentTime.getDate();
-  const currentMonth = currentTime.getMonth() + 1;
-  const currentYear = currentTime.getFullYear();
-  const century = parseFloat(currentYear.toString().slice(0, 2));
-  const year = parseFloat(currentYear.toString().slice(-2));
-  const calMonth = currentTime.toLocaleString('default', { month: 'long' });
-  const firstPart = currentDate + parseFloat((2.6 * currentMonth - 0.2).toString().slice(0, 4));
-  const secondPart = 2.0 * parseFloat(century);
-  const thirdPart = parseFloat((firstPart - secondPart).toString().slice(0, 4));
-  const fourthPart = thirdPart + year + year / 4 + century / 4;
-  const fifthPart = Math.trunc(fourthPart % 7);
-  let i = 0;
-  let x = 1;
-  for (i; i < fifthPart - 1; i++) {
-    y.push(null);
-  }
+  let y = [];
+  let currentTime = new Date();
+  let currentMonth = currentTime.getMonth() + 1;
+  let currentYear = currentTime.getFullYear();
+  let century = parseFloat(currentYear.toString().slice(0, 2));
+  let year = parseFloat(currentYear.toString().slice(-2));
+  let calMonth = currentTime.toLocaleString("default", { month: "long" });
   let monthMax;
   if (currentMonth === 2) {
     if (currentYear % 4 === 0) {
@@ -32,19 +21,40 @@ export default function Calendar() {
       monthMax = 28;
     }
   } else if (
-    currentMonth === 4
-    || currentMonth === 6
-    || currentMonth === 9
-    || currentMonth === 11
+    currentMonth === 4 ||
+    currentMonth === 6 ||
+    currentMonth === 9 ||
+    currentMonth === 11
   ) {
     monthMax = 30;
   } else {
     monthMax = 31;
   }
 
-  while (x !== monthMax + 1) {
-    y[x] = x;
-    x++;
+  if (currentMonth > 2) {
+    currentMonth = currentMonth - 2.0;
+  } else {
+    currentMonth = currentMonth + 10.0;
+  }
+  let firstPart =
+    1.0 + parseFloat((2.6 * currentMonth - 0.2).toString().slice(0, 4));
+  let secondPart = 2.0 * parseFloat(century);
+  let thirdPart = parseFloat((firstPart - secondPart).toString().slice(0, 8));
+  let fourthPart = thirdPart + year + year / 4 + century / 4;
+  let fifthPart = Math.trunc(fourthPart % 7);
+  monthMax = monthMax + fifthPart;
+
+  for (var i = 0; i < fifthPart; i++) {
+    y.push(32);
+  }
+
+  for (var x = 1; x !== monthMax; x++) {
+    y[x + fifthPart - 1] = x;
+  }
+  for (x = 0; x < 7; x++) {
+    if (y[x] === 32) {
+      y[x] = null;
+    }
   }
 
   function daySelected(day) {
