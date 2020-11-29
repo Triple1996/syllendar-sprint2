84635629f7b71b40293630b2273e657bd6d8ad91
Adam A <aaa237@njit.edu>
2020-11-29 19:52:31 +0000

    Remove comment

--- scripts/Content_main.jsx
+++ scripts/Content_main.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Socket } from './Socket';
 import Calendar from './Calendar';
+import ApiCalendar from 'react-google-calendar-api';
+
 
 export function Content_main() {
   const [image, setImage] = React.useState([]);
@@ -20,9 +22,59 @@ export function Content_main() {
   });
 
   function handleSubmit(event) {
+    ApiCalendar.handleSignoutClick();
     ReactDOM.render(<Content />, document.getElementById('content'));
   }
+  
+  function authenticate(response){
+        ApiCalendar.handleAuthClick();
+        console.log(response);
+        console.log("Logged in");
+  }
+    
+  function loadevents(){
+    if (ApiCalendar.sign){
+        ApiCalendar.listUpcomingEvents()
+      .then(
+        ({result}) => {
+                            
+          for (var i = 0; i < result.items.length; i++){
+            let event = result.items[i]
+                        
+            var start;
+            var end;
+                        
+            if (event.start.date){
+              start = "All day"
+              end = "All day"
+            }
+            else{
+              start = event.start.dateTime;
+              end = event.start.dateTime;
+            }
+                        
+            let createdBy = event.creator.email;
+            let id = event.id;
+            let summary = event.summary;
+                        
+            var element = document.createElement("li");
+            var text = document.createTextNode(
+              "created by: " + createdBy + "\n"+
+              "start: "+ start+ "\n"+  // undefined if "All day"
+              "end: "+ end+ "\n"+      // undefined if "All day"
+              "summary: "+ summary);
+            console.log(text);
+          }
+          
+        });
+        
+    }
+    else {
+      window.alert("Must be logged in");
+    }
+  }
 
+  
   return (
     <div>
       <h2>My Calendar</h2>
@@ -32,6 +84,8 @@ export function Content_main() {
         <p>{email}</p>
         <p>{name}</p>
         <div className="buttonpostion">
+        <button onClick={authenticate}> Auth G Calendar </button>
+          <button onClick={loadevents}> Import from Google Calendar </button>
           <form onSubmit={handleSubmit}>
             <button>Sign Out</button>
           </form>
--- scripts/Main.jsx
+++ scripts/Main.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Content } from './Content';
-// import Calendar from './Calendar';
 
 ReactDOM.render(<Content />, document.getElementById('content'));
