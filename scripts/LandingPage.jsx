9c8d78accbac5154d20143a06eb3105805e612fd
Adam A <aaa237@njit.edu>
2020-12-08 19:23:41 +0000

    Add personal info to landing page

--- scripts/LandingPage.jsx
+++ scripts/LandingPage.jsx
@@ -15,7 +15,7 @@ export function LandingPage() {
       <p1>As a team, we aimed to develop a schedule-managing app that is accessible from the browser. What the app does is, it allows the authenticated user to view their calendar. Beyond that, the user can add/remove an event, which will be saved in a database. Also, the user will be able to read a JSON file after it’s being uploaded, and the user will be able to import from an existing Google calendar. Most importantly, this app will help you not miss an important date when you’re overwhelmed with other stuff on your plate.</p1><br /></div>
         <p3>Team Members:</p3><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <div className="firstmember">
-        <img src="/static/picture.PNG" width="150" height="150" className="center"/>
+        <img src="/static/pictureBrijesh.PNG" width="150" height="150" className="center"/>
         <h1>Brijesh Naik</h1>
         <p> Worked with Front-End</p>
         <p>Student at New Jersey Institute of Technology</p>
@@ -23,12 +23,12 @@ export function LandingPage() {
         <a href="https://www.linkedin.com/in/brijesh-naik-563a4a158/"><i className="fa fa-linkedin" ></i> </a>
         </div>
         <div className="secondmember">
-        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width="150" height="150" className="center"/>
+        <img src="/static/pictureAdam.jpg" width="150" height="150" className="center"/>
         <h1>Adam Awany</h1>
         <p> Worked with Back-End</p>
         <p>Student at New Jersey Institute of Technology</p>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-         <a href="#"><i className="fa fa-linkedin" ></i> </a>
+         <a href="https://www.linkedin.com/in/adam-awany/"><i className="fa fa-linkedin" ></i> </a>
         </div>
         <div className="thirdmember">
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width="150" height="150" className="center"/>
