1ed4d847fce39f7fd2b21008e2f7d06e44f1b69b
EC2 Default User <ec2-user@ip-172-31-65-103.ec2.internal>
2020-12-09 05:00:13 +0000

    final commit

--- scripts/UpdateEvent.jsx
+++ scripts/UpdateEvent.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
 
-export default function UpdateEvent() {
+export default function UpdateEvent({ eventInfo }) {
+    console.log(eventInfo);
     return (
-        <div>Update event component here</div>    
+        <div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>Event Name</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.Event}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>Start Time</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.StartTime}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>End Time</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.EndTime}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>Location</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.Location}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>Description</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.Description}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>End Date</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.EndDate}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 text-left">
+                    <p>Event Name</p>
+                </div>
+                <div className="col-8 text-left">
+                    <p>{eventInfo.Event}</p>
+                </div>
+            </div>
+            
+        </div>    
     )
 }
